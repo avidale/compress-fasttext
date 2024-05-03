78f13cf7cd1924a1eb64d4931094225c1b6e7dbc
david <dale.david@mail.ru>
2024-05-03 08:29:26 +0200

    up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="compress-fasttext",
-    version="0.1.4",
+    version="0.1.5",
     author="David Dale",
     author_email="dale.david@mail.ru",
     description="A set of tools to compress gensim fasttext models",
